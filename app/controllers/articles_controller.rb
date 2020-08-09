498db3d6f7db02cd00b52b4e37363158de172857
wataru <wataru.development@gmail.com>
2020-08-09 23:37:13 +0900

    particial テンプレートの活用

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
+    
   end
 
   def new
@@ -22,11 +24,10 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-    @article = Article.find(params[:id])  
+    
   end
 
-  def update
-    @article = Article.find(params[:id])
+  def update   
     if @article.update(article_params)
       redirect_to article_path, notice: "更新できました"
     else
@@ -44,6 +45,10 @@ class ArticlesController < ApplicationController
   private
   def article_params
     params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 
 
