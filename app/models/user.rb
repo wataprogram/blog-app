6fc21c6ea53e42c26e6e8cfd12fe9d45e5fc3d4a
wataru <wataru.development@gmail.com>
2020-09-19 23:33:36 +0900

    no message

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,13 +23,27 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :articles, dependent: :destroy
+  has_one :profile, dependent: :destroy
+
+  delegate :birthday, :gender, :age, to: :profile, allow_nil: true
 
   def has_written?(article)
     articles.exists?(id: article.id)
   end
 
   def display_name
-    # splitでメールアドレスを@で分割する：ハッシュで返す
-    self.email.split('@')[0]
+    profile&.nickname || self.email.split('@')[0]
+  end
+
+  def prepare_profile
+    profile || build_profile
+  end
+
+  def avatar_image
+    if profile&.avatar&.attached?
+      profile.avatar
+    else
+      'default_avator.png'
+    end
   end
 end
